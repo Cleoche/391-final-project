0c5dd12776981d47c75b83cddb86a053432b3a91
Cléo Thor <kiszo.r6@gmail.com>
2024-07-31 13:17:22 -0400

    added comments to Bucket.jsx and BucketContainer.jsx

--- src/components/BucketContainer.jsx
+++ src/components/BucketContainer.jsx
@@ -4,7 +4,7 @@ import {Model} from "./Bucket.jsx";
 export default function BucketContainer(){
     return(
         <>
-            <Canvas>
+            <Canvas>        {/*All animated models must be EXPORTED into a canvas*/}
                 <directionalLight position={[0, 0, 3]}/>
                 <Model position={[0, 0, 0]} color={'green'}/>
             </Canvas>
